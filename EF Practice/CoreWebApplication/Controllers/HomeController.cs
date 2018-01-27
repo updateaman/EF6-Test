58465af342f021e06fdaa562c0932d8264ae0c43
Aman Baloch <aman.baloch@rosslynanalytics.com>
2018-01-27 18:54:55 +0000

    Added context

--- EF Practice/CoreWebApplication/Controllers/HomeController.cs
+++ EF Practice/CoreWebApplication/Controllers/HomeController.cs
@@ -1,41 +1,33 @@
 ﻿using System.Threading.Tasks;
+using CoreWebApplication.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace CoreWebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserDbContext _context;
+
+        public HomeController()
+        {
+            _context = new UserDbContext();
+        }
+
         [HttpPost]
         //[ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("FirstName,LastName,BirthDate,ProfilePhoto,Interests,AboutMe,Address,Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] UserProfile userProfile)
+        public async Task<IActionResult> Edit(string id,
+            [Bind(
+                "FirstName,LastName,BirthDate,ProfilePhoto,Interests,AboutMe,Address")]
+            UserProfile userProfile)
         {
             if (id != userProfile.Id)
             {
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(userProfile);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!UserProfileExists(userProfile.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                //return RedirectToAction(nameof(Index));
-            }
-
+            _context.Update(userProfile);
+            await _context.SaveChangesAsync();
+            
             return Ok();
         }
     }
---
+++ EF Practice/CoreWebApplication/Models/UserDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoreWebApplication.Models
+{
+    public class UserDbContext : DbContext
+    {
+        public DbSet<UserProfile> UserProfiles { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserProfile>()
+                .HasOne(p => p.Address)
+                .WithOne(i => i.UserProfile)
+                .HasForeignKey<Address>(b => b.UserProfileForeignKey);
+        }
+
+    }
+}
