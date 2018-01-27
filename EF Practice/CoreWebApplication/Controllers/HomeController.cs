a8d7d9404732761393fcfe11bf85ec27331cba52
Aman Baloch <aman.baloch@rosslynanalytics.com>
2018-01-27 18:43:45 +0000

    initial commit

---
+++ EF Practice/CoreWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("FirstName,LastName,BirthDate,ProfilePhoto,Interests,AboutMe,Address,Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] UserProfile userProfile)
+        {
+            if (id != userProfile.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userProfile);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserProfileExists(userProfile.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                //return RedirectToAction(nameof(Index));
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ EF Practice/CoreWebApplication/UserProfile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreWebApplication
+{
+    public class UserProfile //: ApplicationUser
+    {
+        [Key]
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string ProfilePhoto { get; set; }
+        public string Interests { get; set; }
+        public string AboutMe { get; set; }
+
+        public Address Address { get; set; }
+
+    }
+
+    public class Address
+    {
+        [Key]
+        public string AddressId { get; set; }
+
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string HouseOrFlatNumber { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+
+        public string UserProfileForeignKey { get; set; }
+        public UserProfile UserProfile { get; set; }
+    }
+}
+
